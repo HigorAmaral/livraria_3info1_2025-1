92ee6beada82eae106e0f0ea4324fd5fe440595a
higoramaral <amarahigor@gmail.com>
2025-07-11 08:41:18 -0300

    Feat: Aula 25, listagem de compra.

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,14 +1,18 @@
-from rest_framework.serializers import CharField, ModelSerializer
+from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField
 
 from core.models import Compra, ItensCompra
 
 
 class ItensCompraSerializer(ModelSerializer):
+    total = SerializerMethodField()
+
+    def get_total(self, instance):
+        return instance.livro.preco * instance.quantidade
+
     class Meta:
         model = ItensCompra
-        fields = ('quantidade', 'livro')
-        depth = 2
-
+        fields = ('livro', 'quantidade', 'total')
+        depth = 1
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source='usuario.email', read_only=True)
